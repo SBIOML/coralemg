2f980dc7c1ee4782d3e3453d5ad982a2e1b72529
etibuteau <etibuteau@gmail.com>
2024-09-02 18:03:48 -0400

    Massive refactor

--- offdevice/save_dataset.py
+++ offdevice/save_dataset.py
@@ -1,241 +1,35 @@
 import os
-import re
-import fnmatch
 import numpy as np
-import scipy.io as sio
-from collections import Counter
 import data_processing as dp
-
-
-def load_emager(path, subject, session, differential=False):
-    """
-    Load EMG data from EMaGer v1 dataset.
-
-    Params:
-        - path : path to EMaGer root
-        - subject : subject id
-        - session : session number
-
-    Returns the loaded data with shape (nb_gesture, nb_repetition, time_length, nb_channels)
-    """
-
-    # Parameters
-    # subject = "01"
-    # session = "1"
-    nb_gesture = 6
-    nb_repetition = 10
-    nb_pts = 5000
-    start_path = "%s/subject_%s/session_%s/" % (path, subject, session)  # ordi UL
-    data_array = np.zeros((nb_gesture, nb_repetition, 64, nb_pts), dtype=int)
-
-    first_file = os.listdir(start_path)[0]
-    arm_used = "right" if "right" in first_file else "left"
-    for gest in range(nb_gesture):
-        for rep in range(nb_repetition):
-            path = (
-                start_path
-                + "0"
-                + subject
-                + "-00"
-                + session
-                + "-00"
-                + str(gest)
-                + "-00"
-                + str(rep)
-                + "-"
-                + arm_used
-                + ".csv"
-            )
-            one_file = np.transpose(np.loadtxt(path, delimiter=","))
-            data_array[gest, rep, :, :] = one_file[:, -nb_pts:]
-    if differential:
-        data_array = np.reshape(data_array, (nb_gesture, nb_repetition, 16, 4, nb_pts))
-        final_array = data_array[:, :, :, 0:3, :] - data_array[:, :, :, 1:4, :]
-        final_array = np.reshape(final_array, (nb_gesture, nb_repetition, 48, nb_pts))
-    else:
-        final_array = data_array
-
-    return np.swapaxes(final_array, 2, 3)
-
-def load_capgmyo(path, subject, session):
-    """
-    Load EMG data from CapgMyo dataset.
-    The format of the folder has to be 
-
-    capgmyo
-├── subject01_session1
-│   ├── 001-001.mat
-│   ├── 001-002.mat
-...
-
-    Params:
-        - path : path to Capgmyo root folder
-        - subject : subject id
-        - session : session number
-
-    Returns the loaded data with shape (nb_gesture, nb_repetition, time_length, nb_channels)
-    """
-
-    # Parameters
-    # subject = "01"
-    # session = "1"
-    dirpath = "%s/subject%s_session%s/" %(path, subject, session)
-    files = fnmatch.filter(os.listdir(dirpath), '*.mat')
-    files = np.sort(files)
-    experiment_list = [] # TODO : See if better method exists
-    for file in files:
-        experiment = sio.loadmat(dirpath+file)
-
-        experiment_list.append(experiment)
-
-    experiment_array = _capgmyo_format_array(experiment_list)
-    
-    return experiment_array
-
-def _capgmyo_format_array(experiment_list):
-    exp_0 = experiment_list[0]
-    data_length, nb_channels = np.shape(exp_0['data'])
-    label_list = []
-    for i, experiment in enumerate(experiment_list):
-        label_list.append(experiment['gesture'][0][0])
-    count = Counter(label_list)
-
-    nb_labels = len(count.values())
-    nb_exp = max(count.values())
-
-    data_array = np.full((nb_labels, nb_exp, data_length, nb_channels), None)
-    curr_exp = 0
-    previous_label = -1
-    for i, experiment in enumerate(experiment_list):
-        curr_data = experiment['data']
-        label = experiment['gesture']
-        if label != previous_label:
-            curr_exp = 0
-        else :
-            curr_exp += 1
-        data_array[label-1,curr_exp,:,:] = curr_data[:,:]
-        previous_label = label
-    return data_array
-
-def load_hyser(path, subject, session):
-    """
-    Load EMG data from Hyser dataset.
-
-    Params:
-        - path : path to Hyser root
-        - subject : subject id
-        - session : session number
-
-    Returns the loaded data with shape (nb_gesture, nb_repetition, time_length, nb_channels)
-    """
-
-    # Parameters
-    # subject = "01"
-    # session = "1"
-
-    # Compiled regex used for gain and baseline
-    RE_GAIN = re.compile(".+?(?=\()")
-    RE_BASELINE = re.compile("(?<=\().*(?=\))")
-
-    task_type = "maintenance"
-    sig_type = "preprocess"
-
-    dirpath = "%s/subject%s_session%s/" %(path, subject, session)
-    nb_files = len(fnmatch.filter(os.listdir(dirpath), '%s_%s_*.dat'%(task_type, sig_type)))
-
-    data = [] # TODO : See if better method exists
-
-    file_name = "%slabel_%s.txt"%(dirpath, task_type)
-    with open(file_name) as fid :
-        labels = np.loadtxt(fid, delimiter =',', dtype = int)
-
-    for i in range(1,nb_files+1):
-        file_name = "%s%s_%s_sample%s"%(dirpath, task_type, sig_type, i)
-        # Import data
-        with open(file_name+".dat", 'rb') as fid:
-            data_array = np.fromfile(fid, dtype=np.int16).reshape(-1,256).astype('float')
-        
-        # Import gain and baseline and applies it to data
-        with open(file_name+".hea",'r') as fid:
-            head_info = np.char.split((fid.readlines()[1:]))
-            for j,line in enumerate(head_info):
-                str_tmp = line[2]
-                gain = float(RE_GAIN.match(str_tmp).group())
-                baseline = float(RE_BASELINE.search(str_tmp).group())
-                data_array[:,j] = (data_array[:,j]-baseline)/gain
-        
-        # Reshape the data
-        data_array = data_array.reshape(-1,4,8,8) 
-        data_array = np.flip(data_array,(2,3))
-        data_array[:,[1,2],:,:] = data_array[:,[2,1],:,:]
-
-
-        data_array = np.concatenate((np.concatenate((data_array[:,0,:,:],data_array[:,1,:,:]),axis = 2),np.concatenate((data_array[:,2,:,:],data_array[:,3,:,:]),axis = 2)),axis=1).reshape(-1,256)
-        
-        data.append(data_array.reshape(-1,256)) # TODO : See if better method exists
-    
-    nb_experiment, time_length, nb_channels = np.shape(data)
-    nb_gesture = len(set(labels))
-    nb_repetition = int(nb_experiment/nb_gesture)
-
-    # First 512 data points are noise
-    time_length = time_length-512
-    experiment_array = np.zeros((nb_gesture, nb_repetition, time_length, nb_channels))
-
-    for i, experiment in enumerate(data):
-        data[i] = experiment[512:,:]
-        curr_label = int(labels[i]-1)
-        experiment_array[curr_label, i%nb_repetition,:,:] = data[i]
-
-    return experiment_array
-
-def load_dataset(path, subject, session):
-    """
-    Load EMG data from given dataset.
-
-    Params:
-        - path : path to EMG dataset
-        - subject : subject id
-        - session : session number
-
-    Returns the loaded data with shape (nb_gesture, nb_repetition, time_length, nb_channels)
-    """
-    if "emager" in path:
-        experiment_array = load_emager(path, subject, session, differential=False)
-    elif "capgmyo" in path:
-        experiment_array = dp.convert_capgmyo_16bit(load_capgmyo(path, subject, session))
-    elif "hyser" in path:
-        experiment_array = dp.convert_hyser_16bit(load_hyser(path, subject, session))
-    else:
-        raise Exception("Supported dataset is not in path")
-    
-    return experiment_array
+import dataset_definition as dtdef
 
 
 def save_training_data(
-    dataset_path, subject, session, compressed_method="minmax", nb_bits=8, window_length=25, dimension=(4,16), save_folder_path=""
+    dataset, dataset_path, subject, session, compressed_method="minmax", nb_bits=8, time_length=25, save_folder_path=""
 ):
     """
     Save the training data for the tensorflow model
 
+    @param dataset the class that represents the dataset
     @param dataset_path the path to the dataset
     @param subject the subject to use, must be 000, 001, ...
     @param session the session to use, must be 001, 002
     @param compressed_method the compression method used
     @param nb_bits the number of bits to compress to
-    @param window_length the length of the time window to use
+    @param time_length the length of the time window to use in ms
     @param dimension the number of electrode in each axis (x,y)
     @param save_folder_path the path of the folder to save the data in
 
     """
+    window_length = int(dataset.sampling_rate*time_length/1000)
+
     main_folder_path = save_folder_path
     if not os.path.exists(main_folder_path):
         os.makedirs(main_folder_path)
 
-    data_array = load_dataset(dataset_path, subject, session)
+    data_array = dataset.load_dataset(dataset_path, subject, session)
 
-    filtering_utility = True if "emager" in dataset_path else False
-    averages_data = dp.preprocess_data(data_array, window_length=window_length, filtering_utility=filtering_utility)
+    averages_data = dp.preprocess_data(data_array, window_length=window_length, filtering_utility=not dataset.utility_filtered)
     X, y = dp.extract_with_labels(averages_data)
 
     X_compressed = dp.compress_data(X, method=compressed_method, residual_bits=nb_bits)
@@ -246,7 +40,7 @@ def save_training_data(
         compressed_method,
         nb_bits
     )
-    X_rolled = dp.roll_data(X_compressed, 2, v_dim=dimension[0], h_dim=dimension[1])
+    X_rolled = dp.roll_data(X_compressed, 2, v_dim=dataset.sensors_dim[0], h_dim=dataset.sensors_dim[1])
 
     # Copy the labels to be the same size as the data
     nb_rolled = int(np.floor(X_rolled.shape[0] / y.shape[0]))
@@ -256,10 +50,11 @@ def save_training_data(
     np.savez(filename, data=X_rolled, label=y_rolled)
 
 
-def save_raw_data(dataset_path, subject, session, save_folder_path="dataset/raw/"):
+def save_raw_data(dataset, dataset_path, subject, session, save_folder_path="dataset/raw/"):
     """
     Save the training data for the tensorflow model
 
+    @param dataset the class that represents the dataset
     @param dataset_path the path to the dataset
     @param subject the subject to use, must be 000, 001, ...
     @param session the session to use, must be 001, 002
@@ -271,11 +66,12 @@ def save_raw_data(dataset_path, subject, session, save_folder_path="dataset/raw/
         os.makedirs(main_folder_path)
 
     filename = "%s/%s_%s_raw.npz" % (main_folder_path, subject, session)
-    data_array = load_dataset(dataset_path, subject, session)
+    data_array = dataset.load_dataset(dataset_path, subject, session)
     np.savez(filename, data=data_array)
 
 
 if __name__ == "__main__":
+    # Emager = dtdef.EmagerDataset()
     # dataset_path = "dataset/emager"
     # subjects = ["00","01","02","03","04","05","06","07","08","09","10", "11"]
     # sessions = ["1", "2"]
@@ -288,6 +84,7 @@ if __name__ == "__main__":
     #         for compressed_method in compressed_methods:
     #             for bit in bits:
     #                 save_training_data(
+    #                     Emager,
     #                     dataset_path,
     #                     subject,
     #                     session,
@@ -295,8 +92,9 @@ if __name__ == "__main__":
     #                     nb_bits=bit,
     #                     save_folder_path="dataset/train/emager/%s" % (compressed_method),
     #                 )
-    #         save_raw_data(dataset_path, subject, session, "dataset/raw/emager/")
+    #         save_raw_data(Emager, dataset_path, subject, session, "dataset/raw/emager/")
 
+    Capgmyo = dtdef.CapgmyoDataset()
     dataset_path = "dataset/capgmyo"
     subjects = ["01","02","03","04","05","06","07","08","09","10"]
     sessions = ["1", "2"]
@@ -309,12 +107,12 @@ if __name__ == "__main__":
             for compressed_method in compressed_methods:
                 for bit in bits:
                     save_training_data(
+                        Capgmyo,
                         dataset_path,
                         subject,
                         session,
                         compressed_method=compressed_method,
                         nb_bits=bit,
-                        dimension=(8,16),
                         save_folder_path="dataset/train/capgmyo/%s" % (compressed_method),
                     )
-            save_raw_data(dataset_path, subject, session, "dataset/raw/capgmyo/")
+            save_raw_data(Capgmyo, dataset_path, subject, session, "dataset/raw/capgmyo/")
--- offdevice/training_model.py
+++ offdevice/training_model.py
@@ -5,11 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import model_definition as md
 import data_processing as dp
+import dataset_definition as dtdef
 
 
 BATCH_SIZE = 64
-AVG_SHAPE = (4, 16, 1)
-NB_CLASSES = 6
 
 def scheduler(epoch, lr):
   if epoch < 5:
@@ -17,11 +16,12 @@ def scheduler(epoch, lr):
   else:
     return lr * tf.math.exp(-0.5)
 
-def train_model(dataset_name, dataset_path, subject="00", session="1", compression_mode="minmax", bit=8, nb_class=6):
+def train_model(dataset, model_name, dataset_path, subject="00", session="1", compression_mode="minmax", bit=8, nb_class=6):
     '''
     Train the model
 
-    @param dataset_name the name of the dataset
+    @param dataset the class that represents the dataset
+    @param model_name the name of the model
     @param dataset_path the path to the dataset
     @param subject the subject to use, must be 00, 01, ...
     @param session the session to use, must be 1, 2
@@ -33,14 +33,13 @@ def train_model(dataset_name, dataset_path, subject="00", session="1", compressi
         tf.config.experimental.set_memory_growth(device, True)
 
 
-    train_dataset, test_dataset = create_training_dataset(dataset_path, subject, session, compression_mode, bit)
+    train_dataset, test_dataset = create_training_dataset(dataset, dataset_path, subject, session, compression_mode, bit)
     
 
     optimizer = tf.keras.optimizers.AdamW(0.00025)
     reduce_lr = tf.keras.callbacks.LearningRateScheduler(scheduler)
 
-
-    model = md.corealemg_net(NB_CLASSES)
+    model = md.coralemg_net(dataset.nb_class)
 
     model.compile(optimizer=optimizer, 
                 loss='sparse_categorical_crossentropy', 
@@ -57,19 +56,19 @@ def train_model(dataset_name, dataset_path, subject="00", session="1", compressi
 
     #generate_history_graph(history)
 
-    model_name = "%s_%s_%s_%s_%sbits"%(dataset_name, subject, session, compression_mode, bit)
+    model_name = "%s_%s_%s_%s_%s_%sbits"%(dataset.name, model_name, subject, session, compression_mode, bit)
 
     saved_keras_model = 'offdevice/model/%s.h5'%(model_name)
     model.save(saved_keras_model)
 
-def fine_tune_model(dataset_name, dataset_path, folder_model_path, model_name):
+def fine_tune_model(dataset, dataset_path, folder_model_path, tuned_name):
     '''
     Fine tune the model
 
-    @param dataset_name the name of the dataset
+    @param dataset the class that represents the dataset
     @param dataset_path the path to the dataset
     @param folder_model_path the path to the folder containing the model
-    @param model_name the name of the model to fine tune
+    @param tuned_name the name of the model to fine tune
     '''
 
     gpu_devices = tf.config.experimental.list_physical_devices("GPU") 
@@ -77,10 +76,10 @@ def fine_tune_model(dataset_name, dataset_path, folder_model_path, model_name):
         tf.config.experimental.set_memory_growth(device, True)
     
 
-    full_model_path = '%s/%s.h5'%(folder_model_path, model_name)
-    subject = model_name.split("_")[1]
-    session = model_name.split("_")[2]
-    compressed_method = model_name.split("_")[3]
+    full_model_path = '%s/%s.h5'%(folder_model_path, tuned_name)
+    subject = tuned_name.split("_")[1]
+    session = tuned_name.split("_")[2]
+    compressed_method = tuned_name.split("_")[3]
 
     fine_tuning_session = "2" if session == "1" else "1"
 
@@ -89,10 +88,10 @@ def fine_tune_model(dataset_name, dataset_path, folder_model_path, model_name):
     with np.load(raw_dataset_path) as data:
         raw_data = data['data']
 
-    filtering_utility = True if "emager" in dataset_name else False
-    window_length = 50 if "hyser" in dataset_name else 25
+    time_length = 25
+    window_length = int(dataset.sampling_rate*time_length/1000)
     
-    averages_data = dp.preprocess_data(raw_data, window_length=window_length, filtering_utility=filtering_utility)
+    averages_data = dp.preprocess_data(raw_data, window_length=window_length, filtering_utility=not dataset.utility_filtered)
 
     for i in range(5):
         # load the model
@@ -118,20 +117,20 @@ def fine_tune_model(dataset_name, dataset_path, folder_model_path, model_name):
 
         fine_tuning_data = averages_data[:,fine_tuning_range,:,:]
         testing_data = averages_data[:,testing_range,:,:]
-        train_dataset, test_dataset = create_tuning_dataset(fine_tuning_data, testing_data, compressed_method, BATCH_SIZE)
+        train_dataset, test_dataset = create_tuning_dataset(dataset, fine_tuning_data, testing_data, compressed_method, BATCH_SIZE)
 
         history = model.fit(train_dataset,
                             epochs=10,
                             validation_data=test_dataset)
         
-        tuned_model_name = model_name + "_tuned_%s_%s"%(fine_tuning_range[0], fine_tuning_range[-1])
+        tuned_model_name = tuned_name + "_tuned_%s_%s"%(fine_tuning_range[0], fine_tuning_range[-1])
         saved_keras_model = 'offdevice/model/tuned/%s.h5'%(tuned_model_name)
         model.save(saved_keras_model)
 
         #generate_history_graph(history)
 
 
-def create_training_dataset(dataset_path, subject="00", session="1", compression_mode="minmax", bit=8):
+def create_training_dataset(dataset, dataset_path, subject="00", session="1", compression_mode="minmax", bit=8):
 
     test_session = "2" if session == "1" else "1"
 
@@ -143,13 +142,15 @@ def create_training_dataset(dataset_path, subject="00", session="1", compression
         y_train = data['label']
 
     with np.load(test_dataset_path) as data:
-        
-        X_test = data['data'][24000:36000,:]
-        y_test = data['label'][24000:36000]
+        nb_of_data = np.shape(data['label'])[0]
+        lw_bound = int(2*nb_of_data/5)
+        hp_bound = int(3*nb_of_data/5)
+        X_test = data['data'][lw_bound:hp_bound,:]
+        y_test = data['label'][lw_bound:hp_bound]
 
     SHUFFLE_BUFFER_SIZE = len(y_train)
-    X_train = X_train.astype('float32').reshape(-1,4,16,1)
-    X_test = X_test.astype('float32').reshape(-1,4,16,1)
+    X_train = X_train.astype('float32').reshape(-1,dataset.sensors_dim[0],dataset.sensors_dim[1],1)
+    X_test = X_test.astype('float32').reshape(-1,dataset.sensors_dim[0],dataset.sensors_dim[1],1)
 
     train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
     train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
@@ -159,15 +160,15 @@ def create_training_dataset(dataset_path, subject="00", session="1", compression
 
     return train_dataset, test_dataset
 
-def create_tuning_dataset(finetuning_data, testing_data, compression_method, batch_size=64):
+def create_tuning_dataset(dataset, finetuning_data, testing_data, compression_method, batch_size=64):
     X_train, y_train = dp.extract_with_labels(finetuning_data)
     X_test, y_test = dp.extract_with_labels(testing_data)
 
     X_train = dp.compress_data(X_train, method=compression_method)
-    X_train = X_train.astype('float32').reshape(-1,4,16,1)
+    X_train = X_train.astype('float32').reshape(-1,dataset.sensors_dim[0],dataset.sensors_dim[1],1)
 
     X_test = dp.compress_data(X_test, method=compression_method)
-    X_test = X_test.astype('float32').reshape(-1,4,16,1)
+    X_test = X_test.astype('float32').reshape(-1,dataset.sensors_dim[0],dataset.sensors_dim[1],1)
 
     y_train = np.array(y_train, dtype=np.uint8)
     y_test = np.array(y_test, dtype=np.uint8)
@@ -206,33 +207,35 @@ def generate_history_graph(history):
     plt.xlabel('epoch')
     plt.show()
 
-def train_all_subjects(dataset_name, dataset_path, subjects, compression_methods, bits):
+def train_all_subjects(dataset, model_name, dataset_path, subjects, compression_methods, bits):
     for subject in subjects:
         for j in range(2):
             for compression in compression_methods:
                 for bit in bits:
                     data_path_compressed = dataset_path+"%s"%(compression)
                     session = str(j+1)
-                    train_model(dataset_name, data_path_compressed, subject, session, compression, bit)
+                    train_model(dataset, model_name, data_path_compressed, subject, session, compression, bit)
 
-def finetune_all_subjects(dataset_name, dataset_path, folder_model_path, compression_methods):
-    for i in range(13):
+def finetune_all_subjects(dataset, model_name, dataset_path, subjects, folder_model_path, compression_methods):
+    for subject in subjects:
         for j in range(2):
             for compression in compression_methods:
                 for bit in bits:
-                    subject = "0" + str(i) if i < 10 else str(i)
                     session = str(j+1)
-                    model_name = "%s_%s_%s_%s_%sbits"%(dataset_name, subject, session, compression, bit)
-                    fine_tune_model(dataset_name, dataset_path, folder_model_path, model_name, bit)
+                    tuned_name = "%s_%s_%s_%s_%s_%sbits"%(dataset_name, model_name, subject, session, compression, bit)
+                    fine_tune_model(dataset, dataset_path, folder_model_path, tuned_name, bit)
 
 if __name__ == "__main__":
+
+    Emager = dtdef.EmagerDataset()
+    dataset_name = Emager.name
     subjects = ["00","01","02","03","04","05","06","07","08","09","10", "11"]
-    dataset_name = "emager"
+    model_name = "cnn"
     train_dataset_path= 'dataset/train/%s/'%(dataset_name)
     #compression_methods = ["baseline", "minmax", "msb", "smart", "root"]
     compression_methods = ["minmax", "msb", "smart", "root"]
     bits = [4,5,6,7,8]
-    train_all_subjects(dataset_name, train_dataset_path, compression_methods, bits)
+    train_all_subjects(Emager, model_name, train_dataset_path, subjects, compression_methods, bits)
 
 
     # raw_dataset_path = 'dataset/raw/%s'%(dataset_name)
